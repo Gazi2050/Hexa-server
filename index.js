b95d16fa20ae51559f705e297bcb70cfaf5da54e
Gazi2050 <gazi45344@gmail.com>
2024-04-17 12:36:08 +0600

    update blog api

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
             res.send(result);
         })
 
+        //blog related api
+        app.post('/blogs', async (req, res) => {
+            const event = req.body;
+            console.log(event);
+            const result = await eventCollection.insertOne(event);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
